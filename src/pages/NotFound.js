fdfadcd36a84ea5a2ea56078207d839bb8490a32
Chantel Carter <chantel.alexia@gmail.com>
2024-04-28 21:26:12 -0700

    not found page and footer styling

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -5,12 +5,14 @@ import { NavLink } from "react-router-dom";
 const NotFound = () => {
   return (
     <>
-      <h1>404: Not Found</h1>
-      <p>We're sorry. The page you are looking for does not exist or has been removed. 
-      <NavLink to="/" className="not-found-link">
-        Please return to the homepage.
-      </NavLink>
-      </p>
+      <div className='notfound-container'>
+        <h1>404</h1>
+        <p>We're sorry! The page you are looking for does not exist or has been removed.</p>
+          <NavLink to="/" className="not-found-link">
+            Please return to the homepage
+          </NavLink>
+      </div>
+      
     </>
   )
 }
