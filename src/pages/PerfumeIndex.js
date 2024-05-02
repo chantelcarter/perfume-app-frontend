b1601a115a324895cda62f54952aaf30213539b7
Chantel Carter <chantel.alexia@gmail.com>
2024-05-01 18:55:28 -0700

    PerfumeIndex item styling

--- src/pages/PerfumeIndex.js
+++ src/pages/PerfumeIndex.js
@@ -11,26 +11,22 @@ const PerfumeIndex = ({ perfumes }) => {
           {perfumes?.map((perfume, index) => {
             return (
               <div className="index-content">
-                <div>
-                  <img
-                    src={perfume.image}
-                    alt="perfume"
-                    className="item-image"
-                  />
-                </div>
-                <div className="item-info">
-                  <p>
-                    <b>{perfume.name}</b>
-                  </p>
-                  <p>by {perfume.designer}</p>
-                  <p>Scent Category: {perfume.category}</p>
-                  <NavLink
-                    to={`/perfumeshow/${perfume.id}`}
-                    className="nav-link"
-                  >
-                    <Button className="card-button">More Details</Button>
-                  </NavLink>
-                </div>
+                <NavLink
+                  to={`/perfumeshow/${perfume.id}`}
+                  className="nav-link"
+                >
+                  <div>
+                    <img
+                      src={perfume.image}
+                      alt="perfume"
+                      className="item-image"
+                    />
+                  </div>
+                  <div className="item-info">
+                    <h5>{perfume.name}</h5>
+                    <p>by {perfume.designer}</p>
+                  </div>
+                </NavLink>
               </div>
             );
           })}
